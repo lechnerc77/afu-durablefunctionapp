253a4ebd3f64bc84ffc5667df8b929bf73cf0e16
Christian Lechner <lechnerc77@users.noreply.github.com>
2020-12-06 09:51:52 +0100

    cleanup files

--- HelloCity/index.ts
+++ HelloCity/index.ts
@@ -1,7 +1,7 @@
 ﻿import { AzureFunction, Context } from "@azure/functions"
 
 const activityFunction: AzureFunction = async function (context: Context): Promise<string> {
-    return `Hello ${context.bindings.name}!`;
-};
+    return `Hello ${context.bindings.name}!`
+}
 
-export default activityFunction;
+export default activityFunction
--- PurchaseInfoOrchestrator/index.ts
+++ PurchaseInfoOrchestrator/index.ts
@@ -6,8 +6,8 @@ const orchestrator = df.orchestrator(function* (context) {
     const materialGroup: MaterialGroupData = yield context.df.callActivity("GetPoDetails", context.bindingData.input)
 
     context.bindingData.input.materialGroupId = materialGroup.materialGroupId
-    
-    const productGroupInfo: ProductGroupInfo = yield context.df.callActivity("GetPgText", context.bindingData.input ) 
+
+    const productGroupInfo: ProductGroupInfo = yield context.df.callActivity("GetPgText", context.bindingData.input)
 
     return productGroupInfo
 
